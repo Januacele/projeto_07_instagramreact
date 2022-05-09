dea0a73e9cc9d0a6ee366f6fe1767bea6fa91bfb
Januacele <januacelevieira@hotmail.com>
2022-05-09 11:37:46 -0300

    Organizacao_Sidebar

--- src/MainContent/Sidebar.js
+++ src/MainContent/Sidebar.js
@@ -1,90 +1,52 @@
-export default function Sidebar() {
+import Sugestao from "./Sugestao";
+
+const sugestoes = [
+    { imagem: "assets/img/bad.vibes.memes.svg", usuario: "bad.vibes.memes", info: "Segue você" },
+    { imagem: "assets/img/chibirdart.svg", usuario: "chibirdart", info: "Segue você" },
+    { imagem: "assets/img/razoesparaacreditar.svg", usuario: "razoesparaacreditar", info: "Novo no Instagram" },
+    { imagem: "assets/img/adorable_animals.svg", usuario: "adorable_animals", info: "Segue você" },
+    { imagem: "assets/img/smallcutecats.svg", usuario: "smallcutecats", info: "Segue você" }
+]
+
+
+const RenderSugestao = () => sugestoes.map(({imagem, usuario, info}, index) => 
+<Sugestao 
+key = {index}
+imagem = {imagem}
+usuario = {usuario}
+info = {info}
+/>
+)
+
+export default function Sidebar () {
     return (
-        
-        <div class="sidebar">
-            <div class="usuario">
-                <img src="assets/img/catanacomics.svg" />
-                <div class="texto">
+        <div className="sidebar">
+            <div className="usuario">
+                <img src="assets/img/catanacomics.svg" alt = "user"/>
+                <div className="texto">
                     <strong>catanacomics</strong>
                     Catana
                 </div>
             </div>
 
-            <div class="sugestoes">
-                <div class="titulo">
+            <div className="sugestoes">
+                <div className="titulo">
                     Sugestões para você
                     <div>Ver tudo</div>
                 </div>
 
-                <div class="sugestao">
-                    <div class="usuario">
-                        <img src="assets/img/bad.vibes.memes.svg" />
-                        <div class="texto">
-                            <div class="nome">bad.vibes.memes</div>
-                            <div class="razao">Segue você</div>
-                        </div>
-                    </div>
-
-                    <div class="seguir">Seguir</div>
-                </div>
-
-                <div class="sugestao">
-                    <div class="usuario">
-                        <img src="assets/img/chibirdart.svg" />
-                        <div class="texto">
-                            <div class="nome">chibirdart</div>
-                            <div class="razao">Segue você</div>
-                        </div>
-                    </div>
-
-                    <div class="seguir">Seguir</div>
-                </div>
-
-                <div class="sugestao">
-                    <div class="usuario">
-                        <img src="assets/img/razoesparaacreditar.svg" />
-                        <div class="texto">
-                            <div class="nome">razoesparaacreditar</div>
-                            <div class="razao">Novo no Instagram</div>
-                        </div>
-                    </div>
-
-                    <div class="seguir">Seguir</div>
-                </div>
-
-                <div class="sugestao">
-                    <div class="usuario">
-                        <img src="assets/img/adorable_animals.svg" />
-                        <div class="texto">
-                            <div class="nome">adorable_animals</div>
-                            <div class="razao">Segue você</div>
-                        </div>
-                    </div>
-
-                    <div class="seguir">Seguir</div>
-                </div>
-
-                <div class="sugestao">
-                    <div class="usuario">
-                        <img src="assets/img/smallcutecats.svg" />
-                        <div class="texto">
-                            <div class="nome">smallcutecats</div>
-                            <div class="razao">Segue você</div>
-                        </div>
-                    </div>
-
-                    <div class="seguir">Seguir</div>
-                </div>
+                <RenderSugestao />
             </div>
 
-            <div class="links">
+            <div className="links">
                 Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma
             </div>
 
-            <div class="copyright">
+            <div className="copyright">
                 © 2021 INSTAGRAM DO FACEBOOK
             </div>
         </div>
-    
-    );
+    )
 }
+
+
---
+++ src/MainContent/Sugestao.js
@@ -0,0 +1,18 @@
+export default function Sugestao ({imagem, usuario, info}) {
+    return (
+
+        <div className="sugestao">
+            <div className="usuario">
+                <img src={imagem} alt={usuario} />
+                <div className="texto">
+                    <div className="nome">{usuario}</div>
+                    <div className="razao">{info}</div>
+                </div>
+            </div>
+
+
+            <div className="seguir">Seguir</div>
+        </div>
+
+    );
+}
